0755803f7082a8cc8c6a7b82137191266980c700
Sebastian Wojtczak <wojtczaksebastian@gmail.com>
2012-10-27 16:41:51 +0200

    Initialized rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atut::Application.config.secret_token = '00334f1c9a2428becc135c3bcc20bca2460c2c74f72dbbe13c63547cb8360a1bc73e171ecf92400ef924f896f196f20106e37b5b7e56116a9c2d5af7d7f3d621'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Atut::Application.config.session_store :cookie_store, key: '_atut_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Atut::Application.config.session_store :active_record_store
